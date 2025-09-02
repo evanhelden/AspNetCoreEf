cedfb223063598ebcbaaf6bd20c7397e08731a05
evanhelden <vanhelden@engineer.com>
2025-09-02 15:23:05 -0500

    Update AppDbContext.cs

--- ChefsRegistry/Repository/AppDbContext.cs
+++ ChefsRegistry/Repository/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using ChefsRegistry.Models;
+using ChefsRegistry.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -10,7 +10,7 @@ namespace ChefsRegistry.Repository
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<ChefModel> Chefs { get; set; }
+        public DbSet<ChefModel> tbl_Chefs { get; set; }
         public DbSet<AddressModel> Address { get; set; }
         public DbSet<PhoneModel> Phone { get; set; }
         public DbSet<RestaurantModel> Restaurant { get; set; }
